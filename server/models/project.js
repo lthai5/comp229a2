ddece41dc1d96c2d0ad2b4479c08af31802da9c9
Lagrange <lthai5@my.centennialcollege.ca>
2024-06-21 22:44:50 -0400

    CRUD/REST API for projects is completed

---
+++ server/models/project.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    startDate : {
+        type : Date,
+        required : true
+    },
+    endDate : {
+        type : Date,
+        required : true
+    },
+});
+
+module.exports = mongoose.model('Project', projectSchema);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+//Routes
+const projectRoutes = require('./routes/project');
+
+//instantiate my db
+mongoose.connect('mongodb+srv://lthai5:Password1@cluster0.7igjstm.mongodb.net/projects');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,9 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/data', (req, res) => {
-    res.json({message: 'Hello from server'})
-});
+app.use('/api/projects', projectRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
